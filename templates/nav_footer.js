524b328ef6c25e3b9a8cc32493f2425a13da82e9
Filip Lundberg <85616077+Filuren123@users.noreply.github.com>
2022-04-25 21:26:42 +0200

    Läs nedan
    Bytte sida till en byggnads sida. Bytte lite färger och omdesigna navbaren samt skrev om ett par rubriker.

--- templates/nav_footer.js
+++ templates/nav_footer.js
@@ -1,13 +1,4 @@
 const sidNaven = document.querySelector("nav.naven");
-const theNav = `
-<div class="img-container">
-    <a href="/index.html">
-        <img src="/img/logo/ltg_logo_round.png" alt="LTG logo">
-    </a>
-</div>
-<div class="anchors">
-    <a href="#">Startsida</a><a href="#">Program</a><a href="#">Om oss</a>
-</div>
-`;
+const theNav = ``;
 
-sidNaven.innerHTML = theNav;
+// sidNaven.innerHTML = theNav;
