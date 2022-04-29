5f7ac57352782c43ce88b56dc794ede4dbb47284
Filip Lundberg <fillun0514@staden.gotheborg.net>
2022-04-29 09:09:29 +0200

    Skapade en header; påbörjade en footer och en varukorg

--- templates/nav_footer.js
+++ templates/nav_footer.js
@@ -1,4 +1,20 @@
-const sidNaven = document.querySelector("nav.naven");
-const theNav = ``;
+const theHeader = document.querySelector(".the_header");
+const headerContent = `
+    <h1><a href="/index.html">Byggnads Kompaniet</a></h1>
+    <nav class="naven">
+        <div class="inner_nav">
+            <div class="nav1">
+                <a href="#">SORTIMENT</a><a href="#">VARUHUSINFO</a>
+            </div>
+            <div class="search">
+                <input type="text" placeholder="Vad letar du efter?">
+            </div>
+            <div class="nav2">
+                <a href="#">MITT KONTO</a>
+                <a href="/varukorg.html">VARUKORG</a>
+            </div>
+        </div>
+    </nav>
+`;
 
-// sidNaven.innerHTML = theNav;
+theHeader.innerHTML = headerContent;
