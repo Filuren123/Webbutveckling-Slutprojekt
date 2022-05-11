38ab99a269c10e4e5e5506c8d48ad3802de6e368
Filip Lundberg <85616077+Filuren123@users.noreply.github.com>
2022-05-11 21:32:01 +0200

    Fixade lite nya images och kod + lite annat

--- js/sortiment_category_handler.js
+++ js/sortiment_category_handler.js
@@ -1,4 +1,4 @@
-const toggleCategory = (element) => {
+const toggleCategory = element => {
     let index;
     for (let i = 0; i < topCategories.length; i++) {
         if (element == topCategories[i]) {
@@ -7,10 +7,12 @@ const toggleCategory = (element) => {
         }
     }
     subCategories[index].classList.toggle("sub_category_open");
+    arrowOnCategories[index].classList.toggle("dropdown_arrow_active");
 }
 
 const topCategories = document.querySelectorAll(".category");
 const subCategories = document.querySelectorAll(".sub_category");
+const arrowOnCategories = document.querySelectorAll(".dropdown_arrow");
 
 subCategories.forEach(element => {
     element.style.display = "none";
